3dc167636c5a6576eb486d69f2a5873a64d3fae7
Circle OS <circleonecapital@gmail.com>
2025-10-16 23:22:44 +0530

    Create jit.rs

---
+++ vm/src/jit.rs
@@ -0,0 +1,6 @@
+// JIT stub - placeholder for future Cranelift/LLVM integration.
+// This file exists to show project structure and intended extension points.
+
+pub fn compile_hot_path(_bytecode: &[u8]) {
+    // future: use cranelift to generate native code
+}
